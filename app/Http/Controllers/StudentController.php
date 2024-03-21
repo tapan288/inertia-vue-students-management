585bf6300caf30831b19f7396076d3f6a2dad06f
Tapan Sharma <tapansharma69@gmail.com>
2024-03-21 14:11:05 +0545

    work on the record update flow

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use App\Http\Resources\ClassesResource;
 use App\Http\Resources\StudentResource;
 use App\Http\Requests\StoreStudentRequest;
+use App\Http\Requests\UpdateStudentRequest;
 
 class StudentController extends Controller
 {
@@ -32,6 +33,23 @@ class StudentController extends Controller
     public function store(StoreStudentRequest $request)
     {
         Student::create($request->validated());
+
+        return redirect()->route('students.index');
+    }
+
+    public function edit(Student $student)
+    {
+        $classes = ClassesResource::collection(Classes::all());
+
+        return inertia('Students/Edit', [
+            'classes' => $classes,
+            'student' => StudentResource::make($student),
+        ]);
+    }
+
+    public function update(UpdateStudentRequest $request, Student $student)
+    {
+        $student->update($request->validated());
 
         return redirect()->route('students.index');
     }
