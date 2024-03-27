83504d92efddd5cb91f8518551b2314391482326
Tapan Sharma <tapansharma69@gmail.com>
2024-03-27 21:17:52 +0545

    implement role create/update flow

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Role;
 use App\Http\Resources\RoleResource;
+use App\Http\Requests\StoreRoleRequest;
+use App\Http\Requests\UpdateRoleRequest;
 
 class RoleController extends Controller
 {
@@ -14,6 +16,32 @@ class RoleController extends Controller
         return inertia('Role/Index', [
             'roles' => $roles,
         ]);
+    }
+
+    public function create()
+    {
+        return inertia('Role/Create');
+    }
+
+    public function store(StoreRoleRequest $request)
+    {
+        Role::create($request->validated());
+
+        return redirect()->route('roles.index');
+    }
+
+    public function edit(Role $role)
+    {
+        return inertia('Role/Edit', [
+            'role' => RoleResource::make($role),
+        ]);
+    }
+
+    public function update(UpdateRoleRequest $request, Role $role)
+    {
+        $role->update($request->validated());
+
+        return redirect()->route('roles.index');
     }
 
     public function destroy(Role $role)
---
+++ resources/js/Pages/Role/Edit.vue
@@ -0,0 +1,87 @@
+<script setup>
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import { Head, Link, useForm, usePage } from "@inertiajs/vue3";
+import InputError from "@/Components/InputError.vue";
+
+const role = usePage().props.role;
+
+const form = useForm({
+    title: role.data.title,
+});
+
+const submit = () => {
+    form.put(route("roles.update", role.data.id), {
+        preserveScroll: true,
+    });
+};
+</script>
+
+<template>
+    <Head title="Edit Role" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Edit Role
+            </h2>
+        </template>
+        <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+            <div class="lg:grid lg:grid-cols-12 lg:gap-x-5">
+                <div class="space-y-6 sm:px-6 lg:px-0 lg:col-span-12">
+                    <form @submit.prevent="submit">
+                        <div class="shadow sm:rounded-md sm:overflow-hidden">
+                            <div class="bg-white py-6 px-4 space-y-6 sm:p-6">
+                                <div>
+                                    <h3
+                                        class="text-lg leading-6 font-medium text-gray-900"
+                                    >
+                                        Role Information
+                                    </h3>
+                                    <p class="mt-1 text-sm text-gray-500">
+                                        Use this form to create a new Role.
+                                    </p>
+                                </div>
+
+                                <div class="grid grid-cols-6 gap-6">
+                                    <div class="col-span-6 sm:col-span-3">
+                                        <label
+                                            for="title"
+                                            class="block text-sm font-medium text-gray-700"
+                                            >Title</label
+                                        >
+                                        <input
+                                            v-model="form.title"
+                                            type="text"
+                                            id="title"
+                                            class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                        />
+                                        <InputError
+                                            class="mt-2"
+                                            :message="form.errors.title"
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                            <div
+                                class="px-4 py-3 bg-gray-50 text-right sm:px-6"
+                            >
+                                <Link
+                                    href="#"
+                                    class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                >
+                                    Cancel
+                                </Link>
+                                <button
+                                    type="submit"
+                                    class="ml-4 bg-indigo-600 border border-transparent rounded-md shadow-sm py-2 px-4 inline-flex justify-center text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                >
+                                    Update
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
